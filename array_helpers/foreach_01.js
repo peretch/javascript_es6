ecdcc8b9c928aa316644175d3b4feba0dea45df6
peretch <sebastian@peretch.com>
2019-11-26 12:23:25 -0300

    Added forEach basic example

---
+++ array_helpers/foreach_01.js
@@ -0,0 +1,10 @@
+const users = [
+    { name: 'cperez', age: 35 },
+    { name: 'smenendez', age: 24 },
+    { name: 'fhernandez', age: 22 },
+];
+
+users.forEach((user) => {
+    // DO stuff
+    console.log(user.name);
+});
